055419f0c42e913afba57994ba9b82044ab2b6b7
Akula Guru Datta <40553773+datta07@users.noreply.github.com>
2021-10-20 23:45:25 +0530

    Update models.py

--- price_history/models.py
+++ price_history/models.py
@@ -3,7 +3,6 @@ import json
 
 class ProductPrice:
     """Stores the price of a product at a given time"""
-
     def __init__(self, date, price: float):
         self.date = date
         self.price = price
@@ -11,26 +10,30 @@ class ProductPrice:
 
 class ProductDetails:
     """Stores name, current price, lowest price, highest price and history of the prices of the product"""
-
-    def __init__(self, uid, name, current_price, lowest_price, highest_price, average_price, store, image, stock):
+    def __init__(self, uid, name, current_price, lowest_price, highest_price, store,most_frequent_price,average_price):
         self.id = uid
         self.name = name
         self.current_price = current_price
         self.lowest_price = lowest_price
         self.highest_price = highest_price
-        self.average_price = average_price
         self.store = store
-        self.image = image
-        self.inStock = stock
-    @staticmethod
-    def __convert_price(price: str) -> float:
-        return float(price.replace(',', ''))
+        self.most_frequent_price=most_frequent_price
+        self.average_price=average_price
 
     @staticmethod
     def fromJson(jsonData):
         data = json.loads(jsonData)["effects"]["emits"][0]["params"][0]
-
-        return ProductDetails(data["id"], data["title"], ProductDetails.__convert_price(data["curr_price"]),
-                              ProductDetails.__convert_price(data["low_price"]),
-                              ProductDetails.__convert_price(data["high_price"]),
-                              ProductDetails.__convert_price(data["avg_price"]), data["store"], data['image'], True if data['stock'] == 1 else False)
+        prices={}
+        sum=0
+        temp_data=json.loads(data["data"])
+        max,maxPoint=1,temp_data[0][1]
+        for i in temp_data:
+            if i[1] in prices:
+                prices[i[1]]+=1
+            else:
+                prices[i[1]]=1
+            if prices[i[1]]>max:
+                max,maxPoint=prices[i[1]],i[1]
+            sum+=i[1]
+        avg=round(sum/len(temp_data),2)
+        return ProductDetails(data["id"], data["title"], data["curr_price"], data["low_price"], data["high_price"], data["store"],maxPoint,avg)
